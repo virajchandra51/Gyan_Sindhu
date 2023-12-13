b65d8838ed92c5472b60d7aa49c08d09c37e886a
Viraj Chandra <virajchandra51@gmail.com>
2023-12-13 13:01:45 +0530

    contact us page

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -18,7 +18,14 @@ const ContactUs = () => {
           </div>
         </div>
         {/* heading and paragaph end */}
-        
+        <div className="flex justify-center items-center mb-24">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3606.816533900871!2d82.98427457938783!3d25.310367419980036!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x398e320816f58b37%3A0x5b32cbee9fa630ae!2sGyan%20Sindhu!5e0!3m2!1sen!2sin!4v1702452222971!5m2!1sen!2sin"
+            width="800"
+            height="650"
+            referrerpolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </div>
       </Wrapper>
     </Layout>
   );
